bb89cb3d54f9111fffd7a3838999ee009caa1ea2
David-MarCle <martinezclementedavid@gmail.com>
2023-12-12 12:03:19 +0100

    prueba

--- js/prueba.js
+++ js/prueba.js
@@ -6,3 +6,20 @@ function changeVentana() {
     } else {
       img.src = "img/windowColor.png";
     }}
+
+    function changeImg() {
+        let img = document.getElementById("fotoPared2a");
+    
+        if (img.src.match("Color")) {
+            img.src = "img/fotoPared2.png";
+        } else {
+          img.src = "img/fotoParedColor2a.png";
+        }}
+        function changeB() {
+            let img = document.getElementById("fotoPared2b");
+        
+            if (img.src.match("Color")) {
+                img.src = "img/fotoPared2.png";
+            } else {
+              img.src = "img/fotoParedColor2b.png";
+            }}
