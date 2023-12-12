05e44087ae8ae8211f6c3944d3f23552671d6c26
David-MarCle <martinezclementedavid@gmail.com>
2023-12-12 14:43:34 +0100

    acordeon

--- js/prueba.js
+++ js/prueba.js
@@ -1,25 +1,118 @@
-function changeVentana() {
-    let img = document.getElementById("imgVentana");
+
+function changeFondo() {
+    let img = document.getElementById("imgFondo");
 
     if (img.src.match("Color")) {
-        img.src = "img/windowNegra.png";
+      img.src = "img/fondoPared.png";
     } else {
-      img.src = "img/windowColor.png";
-    }}
-
-    function changeImg() {
-        let img = document.getElementById("fotoPared2a");
-    
+      img.src = "img/fondoParedColor.png";
+    }
+    function changeInter() {
+        let img = document.getElementById("imgInterruptor");
+      
         if (img.src.match("Color")) {
-            img.src = "img/fotoPared2.png";
+          img.src = "img/interruptorPared.png";
         } else {
-          img.src = "img/fotoParedColor2a.png";
-        }}
-        function changeB() {
-            let img = document.getElementById("fotoPared2b");
-        
-            if (img.src.match("Color")) {
-                img.src = "img/fotoPared2.png";
-            } else {
-              img.src = "img/fotoParedColor2b.png";
-            }}
+          img.src = "img/interruptorParedColor.png";
+        }
+      }
+}function changeVentana() {
+  let img = document.getElementById("imgVentana");
+
+  if (img.src.match("Color")) {
+    img.src = "img/windowNegra.png";
+  } else {
+    img.src = "img/windowColor.png";
+  }
+}
+
+function changeA() {
+  let img = document.getElementById("fotoPared2a");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2a.png";
+  }
+}
+function changeB() {
+  let img = document.getElementById("fotoPared2b");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2b.png";
+  }
+}
+function changeC() {
+  let img = document.getElementById("fotoPared2c");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2c.png";
+  }
+}
+function changeD() {
+  let img = document.getElementById("fotoPared2d");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2d.png";
+  }
+}
+function changeE() {
+  let img = document.getElementById("fotoPared2e");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2e.png";
+  }
+}
+function changeF() {
+  let img = document.getElementById("fotoPared2f");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2f.png";
+  }
+}
+function changeG() {
+  let img = document.getElementById("fotoPared2g");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2g.png";
+  }
+}
+function changeH() {
+  let img = document.getElementById("fotoPared2h");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2h.png";
+  }
+}
+function changeI() {
+  let img = document.getElementById("fotoPared2i");
+
+  if (img.src.match("Color")) {
+    img.src = "img/fotoPared2.png";
+  } else {
+    img.src = "img/fotoParedColor2i.png";
+  }
+}
+function changeAcordeon() {
+    let img = document.getElementById("imgAcordeon");
+  
+    if (img.src.match("Color")) {
+      img.src = "fotoaudio/audio/acordeonnbn.png";
+    } else {
+      img.src = "fotoaudio/audio/acordeoncolor.png";
+    }
+  }
