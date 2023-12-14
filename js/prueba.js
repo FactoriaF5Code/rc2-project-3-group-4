d6b00ffd701bd95ac1033ad48c47a0560baa7391
mfg1985 <mfg1985@me.com>
2023-12-14 15:37:03 +0100

    reducir codigo

--- js/prueba.js
+++ js/prueba.js
@@ -35,152 +35,39 @@ function changePolaroidImage(letter) {
     img.src = `img/polaroidColor${letter}.png`;
   }
 }
-
-function changeAcordeon() {
-  let img = document.getElementById("imgAcordeon");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/acordeonnbn.png";
-  } else {
-    img.src = "fotoaudio/audio/acordeoncolor.png";
-  }
-}
-function changeBateria() {
-  let img = document.getElementById("imgBateria");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/bateriabn.png";
-  } else {
-    img.src = "fotoaudio/audio/bateriacolor.png";
-  }
-}
-function changeContrabajo() {
-  let img = document.getElementById("imgContrabajo");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/contrabajobn.png";
-  } else {
-    img.src = "fotoaudio/audio/contrabajocolor.png";
-  }
-}
-function changeElectrica() {
-  let img = document.getElementById("imgElectrica");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/electrbn.png";
-  } else {
-    img.src = "fotoaudio/audio/electcolor.png";
-  }
-}
-function changeGuitarra() {
-  let img = document.getElementById("imgGuitarra");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/guitarrabn.png";
-  } else {
-    img.src = "fotoaudio/audio/guitarracolor.png";
-  }
-}
-function changeSaxofon() {
-  let img = document.getElementById("imgSaxofon");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/saxofonbn.png";
-  } else {
-    img.src = "fotoaudio/audio/saxofoncolor.png";
-  }
-}
-function changeTambor() {
-  let img = document.getElementById("imgTambor");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/tamborbn.png";
-  } else {
-    img.src = "fotoaudio/audio/tamborcolor.png";
-  }
-}
-function changeTrompeta() {
-  let img = document.getElementById("imgTrompeta");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/trompetabn.png";
-  } else {
-    img.src = "fotoaudio/audio/trompetacolor.png";
-  }
-}
-function changeViolin() {
-  let img = document.getElementById("imgViolin");
-
-  if (img.src.match("Color")) {
-    img.src = "fotoaudio/audio/violinbn.png";
-  } else {
-    img.src = "fotoaudio/audio/violincolor.png";
-  }
-}
-function reproducirAudioAcordeon() {
-  let audio = new Audio("fotoaudio/audio/acordeon.mp3");
+function reproducirAudio(instru) {
+  let audio = document.getElementById(`img${instru}`);
   audio.currentTime = 0;
   audio.play();
-}
-function reproducirAudioBateria() {
-  let audio = new Audio("fotoaudio/audio/bateria.mp3");
-  audio.currentTime = 0;
-  audio.play();
-}
-function reproducirAudioContrabajo() {
-  let audio = new Audio("fotoaudio/audio/contrabajo.mp3");
-  audio.currentTime = 0;
-  audio.play();
-}
-function reproducirAudioElectrica() {
-  let audio = new Audio("fotoaudio/audio/electrica.mp3");
-  audio.currentTime = 0;
-  audio.play();
-}
-function reproducirAudioGuitarra() {
-  let audio = new Audio("fotoaudio/audio/guitarra.mp3");
-  audio.currentTime = 0;
-  audio.play();
-}
-function reproducirAudioSaxofon() {
-  let audio = new Audio("fotoaudio/audio/saxofon.mp3");
-  audio.currentTime = 0;
-  audio.play();
-}
-function reproducirAudioTrompeta() {
-  let audio = new Audio("fotoaudio/audio/trompeta.mp3");
-  audio.currentTime = 0;
-  audio.play();
-}
-function reproducirAudiotTambor() {
-  let audio = new Audio("fotoaudio/audio/tambor.mp3");
-  audio.currentTime = 0;
-  audio.play();
-}
-function reproducirAudioViolin() {
-  let audio = new Audio("fotoaudio/audio/violin.mp3");
-  audio.currentTime = 0;
-  audio.play();
+  setTimeout(function () {
+    audio.pause();
+  }, 3000);
 }
 
 document.getElementById("imgInterruptor").addEventListener("click", () => {
-  
-  const letters = ["A","B","C","D","E", "F", "G", "H", "I"];
+  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 
   for (const letter of letters) {
     changePolaroidImage(letter);
   }
-  
-  changeVentana();
-  changeFondo();
+});
 
-  changeAcordeon();
-  changeBateria();
-  changeContrabajo();
-  changeElectrica();
-  changeGuitarra();
-  changeSaxofon();
-  changeTambor();
-  changeTrompeta();
-  changeViolin();
+changeVentana();
+changeFondo();
+
+document.getElementById("enlace").addEventListener("click", () => {
+  const instruments = [
+    "Acordeon",
+    "Bateria",
+    "Contrabajo",
+    "Electrica",
+    "Guitarra",
+    "Saxofon",
+    "Tambor",
+    "Trompeta",
+    "Violin",
+  ];
+  for (const instru of instruments) {
+    reproducirAudio(instru);
+  }
 });
